e04018cd589b6b97d4c9da260e0fab13a4dc25aa
Jimcaale Galiixe <Jimcaale Galiixe>
2023-02-11 11:30:31 +0300

    Captilize the later E

--- pythonGestGame.py
+++ pythonGestGame.py
@@ -5,7 +5,7 @@ gest_Limit = 3
 out_ofgest = False
 while gest != secret_word and not(out_ofgest):
     if gest_count < gest_Limit:
-        gest = input("enter the gest word: ")
+        gest = input("Enter the gest word: ")
         gest_count += 1
     else:
         out_ofgest = True
